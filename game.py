69734ef9e1b95121653e65b1adf94ba30d05a0c8
John Pullmann <john.pullmann@gmail.com>
2022-11-06 13:37:18 +0100

    Fix assets bugs and improve graphic
    - Fix png pieces stretching.
    - Switch white and black position
    - Add board2.png
    - Change background color

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ import os
 pygame.init()
 FPS = 60
 BLACK = (0,0,0)
+BACKGROUND_COLOR = (49,46,43)
 
 # create window
 WIDTH, HEIGHT = 1280, 720
@@ -17,8 +18,11 @@ pygame.display.set_icon(icon)
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
+board = "board2"
+
+
 def draw():
-    WINDOW.fill(BLACK)
+    WINDOW.fill(BACKGROUND_COLOR)
     WINDOW.blit(Board.board_image, Board.position_of_board)
     WINDOW.blit(Board.identifiers_image, Board.position_of_identifiers)
 
@@ -30,20 +34,20 @@ def draw():
     pygame.display.update()
 
 def load_images():
-    Board.board_image = pygame.image.load(os.path.join("images/PNG/", "board1.png"))
-    Board.board_image = pygame.transform.scale(Board.board_image, (Board.size_of_board, Board.size_of_board))
+    Board.board_image = pygame.image.load(os.path.join("images/PNG/", f"{board}.png"))
+    Board.board_image = pygame.transform.smoothscale(Board.board_image, (Board.size_of_board, Board.size_of_board))
 
     Board.identifiers_image = pygame.image.load(os.path.join("images/PNG/", "identifiers.png"))
-    Board.identifiers_image = pygame.transform.scale(Board.identifiers_image, (Board.size_of_identifiers, Board.size_of_identifiers))
+    Board.identifiers_image = pygame.transform.smoothscale(Board.identifiers_image, (Board.size_of_identifiers, Board.size_of_identifiers))
 
     for p in Board.white_pieces:
         #print(f"Loading: {p.color}_{p.type.lower()}.png")
         p.image = pygame.image.load(os.path.join("images/PNG/", f"{p.color}_{p.type.lower()}.png")) 
-        p.image = pygame.transform.scale(p.image, (p.size, p.size))
+        p.image = pygame.transform.smoothscale(p.image, (p.size, p.size))
     for p in Board.black_pieces:
         #print(f"Loading: {p.color}_{p.type.lower()}.png")
         p.image = pygame.image.load(os.path.join("images/PNG/", f"{p.color}_{p.type.lower()}.png")) 
-        p.image = pygame.transform.scale(p.image, (p.size, p.size))
+        p.image = pygame.transform.smoothscale(p.image, (p.size, p.size))
 
 
 class Board():
@@ -66,42 +70,42 @@ class Board():
         self.board = [[0 for _ in range(8)] for _ in range(8)]
         print(f"Board: {self.board}")
 
-        self.board[0][0] = Rook("white", (0, 0))
-        self.board[0][1] = Knight("white", (0, 1))
-        self.board[0][2] = Bishop("white", (0, 2))
-        self.board[0][3] = Queen("white", (0, 3))
-        self.board[0][4] = King("white", (0, 4))
-        self.board[0][5] = Bishop("white", (0, 5))
-        self.board[0][6] = Knight("white", (0, 6))
-        self.board[0][7] = Rook("white", (0, 7))
+        self.board[6][0] = Pawn("white", (6, 0))
+        self.board[6][1] = Pawn("white", (6, 1))
+        self.board[6][2] = Pawn("white", (6, 2))
+        self.board[6][3] = Pawn("white", (6, 3))
+        self.board[6][4] = Pawn("white", (6, 4))
+        self.board[6][5] = Pawn("white", (6, 5))
+        self.board[6][6] = Pawn("white", (6, 6))
+        self.board[6][7] = Pawn("white", (6, 7))
 
-        self.board[1][0] = Pawn("white", (1, 0))
-        self.board[1][1] = Pawn("white", (1, 1))
-        self.board[1][2] = Pawn("white", (1, 2))
-        self.board[1][3] = Pawn("white", (1, 3))
-        self.board[1][4] = Pawn("white", (1, 4))
-        self.board[1][5] = Pawn("white", (1, 5))
-        self.board[1][6] = Pawn("white", (1, 6))
-        self.board[1][7] = Pawn("white", (1, 7))
+        self.board[7][0] = Rook("white", (7, 0))
+        self.board[7][1] = Knight("white", (7, 1))
+        self.board[7][2] = Bishop("white", (7, 2))
+        self.board[7][3] = Queen("white", (7, 3))
+        self.board[7][4] = King("white", (7, 4))
+        self.board[7][5] = Bishop("white", (7, 5))
+        self.board[7][6] = Knight("white", (7, 6))
+        self.board[7][7] = Rook("white", (7, 7))
 
 
-        self.board[6][0] = Pawn("black", (6, 0))
-        self.board[6][1] = Pawn("black", (6, 1))
-        self.board[6][2] = Pawn("black", (6, 2))
-        self.board[6][3] = Pawn("black", (6, 3))
-        self.board[6][4] = Pawn("black", (6, 4))
-        self.board[6][5] = Pawn("black", (6, 5))
-        self.board[6][6] = Pawn("black", (6, 6))
-        self.board[6][7] = Pawn("black", (6, 7))
+        self.board[0][0] = Rook("black", (0, 0))
+        self.board[0][1] = Knight("black", (0, 1))
+        self.board[0][2] = Bishop("black", (0, 2))
+        self.board[0][3] = Queen("black", (0, 3))
+        self.board[0][4] = King("black", (0, 4))
+        self.board[0][5] = Bishop("black", (0, 5))
+        self.board[0][6] = Knight("black", (0, 6))
+        self.board[0][7] = Rook("black", (0, 7))
 
-        self.board[7][0] = Rook("black", (7, 0))
-        self.board[7][1] = Knight("black", (7, 1))
-        self.board[7][2] = Bishop("black", (7, 2))
-        self.board[7][3] = Queen("black", (7, 3))
-        self.board[7][4] = King("black", (7, 4))
-        self.board[7][5] = Bishop("black", (7, 5))
-        self.board[7][6] = Knight("black", (7, 6))
-        self.board[7][7] = Rook("black", (7, 7))
+        self.board[1][0] = Pawn("black", (1, 0))
+        self.board[1][1] = Pawn("black", (1, 1))
+        self.board[1][2] = Pawn("black", (1, 2))
+        self.board[1][3] = Pawn("black", (1, 3))
+        self.board[1][4] = Pawn("black", (1, 4))
+        self.board[1][5] = Pawn("black", (1, 5))
+        self.board[1][6] = Pawn("black", (1, 6))
+        self.board[1][7] = Pawn("black", (1, 7))
 
         #print(f"Board: {self.board}")
 
