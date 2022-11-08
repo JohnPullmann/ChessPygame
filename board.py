77f9beb7c8d6a6f09771dc3acddbada0359487bf
John Pullmann <john.pullmann@gmail.com>
2022-11-08 19:07:48 +0100

    Add movement and turn features.
    - Add player class
    - Add switching turns
    - Add highlighting square under holding piece.
    - Add last turn highlighting
    - Improved movement

--- board.py
+++ board.py
@@ -28,10 +28,13 @@ class Board():
     white_pieces = []
     black_pieces = []
     all_pieces =[]
+    holding_piece = None
     selected_piece = None
-    player1_name = "Player1" # name can be changed later
-    player2_name = "Player2" # name can be changed later
-    
+
+    player1 = None
+    player2 = None
+    player_on_turn = None
+
     position_of_board: tuple = (0, 0)
     size_of_board: int = 0
     board_image = None # image loaded 
@@ -44,7 +47,11 @@ class Board():
 
     selected_square_image = None  # image loaded later
     selected_square_image_original_size = None  # image loaded later
+    holding_over_square_image = None  # image loaded later
+    holding_over_square_image_original_size = None  # image loaded later
     mouse_on_square = [(),()] # [board_pos, board_pos_coords]
+
+    last_turn = [(),()] # [start_board_pos, end_board_pos]
 
     def __init__(self):
 
@@ -92,6 +99,16 @@ class Board():
 
         #print(f"Board: {self.board}")
 
+        Board.player1 = Player("Player1", "white") # player options can be changed later
+        Board.player2 = Player("Player2", "black") # player options can be changed later 
+        if Board.player1.color == "white":
+            Board.player_on_turn = Board.player1
+        elif Board.player2.color == "white":
+            Board.player_on_turn = Board.player2
+        else:
+            print("Problem! Neither player is white!")    
+
+
     def resize_window_reset_pos(self):
         WIDTH, HEIGHT = pygame.display.get_window_size()
         Board.position_of_board = (WIDTH*0.5-(HEIGHT*0.8*0.5), HEIGHT*0.1)
@@ -102,6 +119,25 @@ class Board():
         #print(type(pygame.display.get_window_size()))
 
         self.rec.update(*Board.position_of_board, Board.size_of_board, Board.size_of_board)
+
+    def select_piece(self, mouse_x: float, mouse_y: float) -> None:
+        mouse_board_pos = coords_to_board_pos((mouse_x, mouse_y))
+        if mouse_board_pos != -1:
+            selected_piece = self.board[mouse_board_pos[0]][mouse_board_pos[1]]
+            if selected_piece != None:
+                #print(selected_piece, self.player_on_turn)
+                if selected_piece.color == self.player_on_turn.color:
+                    selected_piece.selected = True
+                    Board.holding_piece = selected_piece
+                    Board.selected_piece = selected_piece
+                else:
+                    Board.selected_piece = None
+                    Board.holding_piece = None
+        else:
+            Board.selected_piece = None
+            Board.holding_piece = None
+
+
 
 
 class Piece(Board):
@@ -129,8 +165,27 @@ class Piece(Board):
         self.size = Board.size_of_board/8
         self.rec.update(*self.coords, self.size, self.size)
     
-    def move(self) -> bool:
-        return True
+    def move(self, dest_board_pos) -> bool:
+        if self.board_pos != dest_board_pos:
+            if Board.player_on_turn.color == self.color:
+                print(f"MOVEMENT, {Board.player_on_turn.name}\nFROM: {self.board_pos}  TO: {dest_board_pos}\n")
+
+                if Board.player_on_turn == Board.player1:
+                    Board.player_on_turn = Board.player2
+                elif Board.player_on_turn == Board.player2:
+                    Board.player_on_turn = Board.player1
+                else:
+                    print("Problem!!!, Program didn't switch players!")
+                print(f"Switched players, player on move: {Board.player_on_turn.name}")
+                Board.last_turn = [self.board_pos, dest_board_pos]
+
+                return True
+
+            else:
+                print(f"You tried to move piece of different color, \nYour color: {Board.player_on_turn.color}, Piece color: {self.color}")
+        else:
+            print(f"Same board pos, not moved \nFROM: {self.board_pos}  TO: {dest_board_pos}\n")
+        return False
 
     def __repr__(self):
         return f"{self.__class__.__name__}({self.color}, {self.board_pos})"
@@ -143,8 +198,8 @@ class Pawn(Piece):
         #print(f"{self.color} Pawn{self.board_pos[1]} position: {self.coords}")
         #print(f"{self.color} Pawn{self.board_pos[1]} size: {self.size}\n")
 
-    def move(self) -> bool:
-        return True
+    #def move(self) -> bool:
+    #    return True
 
 
 class Knight(Piece):
@@ -184,3 +239,8 @@ class Queen(Piece):
         super().__init__(color, board_pos, selected)
         self.type = "Queen"
     
+
+class Player(Board):
+    def __init__(self, name: str, color: str):
+        self.name = name
+        self.color = color
--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import pygame
 import os
-from board import Board, Piece, Pawn, Knight, Bishop, Rook, Queen, King
+from board import Board, Piece, Pawn, Knight, Bishop, Rook, Queen, King, Player
 from board import board_pos_to_coords, coords_to_board_pos
 
 pygame.init()
@@ -31,15 +31,26 @@ def draw() -> None:
     WINDOW.blit(Board.board_image, Board.position_of_board)
     WINDOW.blit(Board.identifiers_image, Board.position_of_identifiers)
 
+    if Board.last_turn[0] != () and Board.last_turn[1] != ():
+        start_turn_board_coords = board_pos_to_coords(Board.last_turn[0])
+        end_turn_board_coords = board_pos_to_coords(Board.last_turn[1])
+
+        WINDOW.blit(Board.selected_square_image, start_turn_board_coords)  
+        WINDOW.blit(Board.selected_square_image, end_turn_board_coords)         
+
     if Board.selected_piece != None:
+        selected_square_board_coords = board_pos_to_coords(Board.selected_piece.board_pos)
+        WINDOW.blit(Board.selected_square_image, selected_square_board_coords)     
+
+    if Board.holding_piece != None:
         mouse_on_square_board_pos, mouse_on_square_board_coords = Board.mouse_on_square
-        if mouse_on_square_board_pos != -1 and mouse_on_square_board_coords != -1:
-            WINDOW.blit(Board.selected_square_image, mouse_on_square_board_coords)
+        if mouse_on_square_board_coords != -1:
+            WINDOW.blit(Board.holding_over_square_image, mouse_on_square_board_coords)
 
     for p in Board.all_pieces:
         WINDOW.blit(p.image, p.coords)
-    if Board.selected_piece != None:
-        WINDOW.blit(Board.selected_piece.image, Board.selected_piece.coords)
+    if Board.holding_piece != None:
+        WINDOW.blit(Board.holding_piece.image, Board.holding_piece.coords)
     
     
     
@@ -50,7 +61,8 @@ def load_images() -> None:
     Board.board_image_original_size = pygame.image.load(os.path.join("images/PNG/", f"{board_color}.png"))
     Board.identifiers_image_original_size = pygame.image.load(os.path.join("images/PNG/", "identifiers.png"))
 
-    Board.selected_square_image_original_size = pygame.image.load(os.path.join("images/PNG/", "selected_square.png"))
+    Board.selected_square_image_original_size = pygame.image.load(os.path.join("images/PNG/", "selected_square.png")) 
+    Board.holding_over_square_image_original_size = pygame.image.load(os.path.join("images/PNG/", "holding_over_square.png"))
 
     for p in Board.all_pieces:
         #print(f"Loading: {p.color}_{p.type.lower()}.png")
@@ -62,8 +74,9 @@ def load_images() -> None:
 def transform_images() -> None:
     Board.board_image = pygame.transform.smoothscale(Board.board_image_original_size, (Board.size_of_board, Board.size_of_board))
     Board.identifiers_image = pygame.transform.smoothscale(Board.identifiers_image_original_size, (Board.size_of_identifiers, Board.size_of_identifiers))
- 
+    
     Board.selected_square_image = pygame.transform.smoothscale(Board.selected_square_image_original_size, (Board.size_of_board/8, Board.size_of_board/8))
+    Board.holding_over_square_image = pygame.transform.smoothscale(Board.holding_over_square_image_original_size, (Board.size_of_board/8, Board.size_of_board/8))
 
     for p in Board.all_pieces:
         p.image = pygame.transform.smoothscale(p.image_original_size, (p.size, p.size))
@@ -78,64 +91,49 @@ def resize_window(board) -> None:
     transform_images() 
 
 
-def select_piece(mouse_x: float, mouse_y: float, board: Board) -> None:
-
-    mouse_board_pos = coords_to_board_pos((mouse_x, mouse_y))
-    if mouse_board_pos != -1:
-        selected_piece = board.board[mouse_board_pos[0]][mouse_board_pos[1]]
-        print(selected_piece)
-    else:
-        selected_piece = None
-    
-    if selected_piece != None:
-        selected_piece.selected = True
-        Board.selected_piece = selected_piece
-        #print(f"Coords: {(mouse_x, mouse_y)}\nBoard position: {mouse_board_pos}\nSelected piece: {selected_piece}")
-
-
-def selected_piece_move_floating(mouse_x: float, mouse_y: float) -> None:
-    if Board.selected_piece != None:
+def holding_piece_move(mouse_x: float, mouse_y: float) -> None:
+    if Board.holding_piece != None:
         x = mouse_x
         y = mouse_y
         x = x if x > Board.position_of_board[0] else Board.position_of_board[0]
         x = x if x < Board.position_of_board[0]+Board.size_of_board else Board.position_of_board[0]+Board.size_of_board
         y = y if y > Board.position_of_board[1] else Board.position_of_board[1]
         y = y if y < Board.position_of_board[1]+Board.size_of_board else Board.position_of_board[1]+Board.size_of_board
-        
-        sel_piece_size = Board.selected_piece.size
-        Board.selected_piece.coords = (x-(sel_piece_size/2), y-(sel_piece_size/2))    
+        holding_piece_size = Board.holding_piece.size
+        x -= holding_piece_size/2
+        y -= holding_piece_size/2
+
+        Board.holding_piece.coords = (x, y)    
 
 
 def selected_piece_drop(mouse_x: float, mouse_y: float, board: Board) -> None:
     if Board.selected_piece != None:
-        sel_piece_board_pos = coords_to_board_pos((mouse_x, mouse_y))
-        if sel_piece_board_pos != -1:
-            sel_piece_pos = board_pos_to_coords(sel_piece_board_pos)
-            
-            if Board.selected_piece.move():
-                print(f"Piece drop position: {sel_piece_board_pos}")
+        new_board_pos = coords_to_board_pos((mouse_x, mouse_y))
+        if new_board_pos != -1:
+            new_piece_pos = board_pos_to_coords(new_board_pos)
+            if Board.selected_piece.move(new_board_pos):
+                #print(f"Piece drop position: {sel_piece_board_pos}")
                 board.board[Board.selected_piece.board_pos[0]][Board.selected_piece.board_pos[1]] = None
-                board.board[sel_piece_board_pos[0]][sel_piece_board_pos[1]] = Board.selected_piece
+                board.board[new_board_pos[0]][new_board_pos[1]] = Board.selected_piece
 
-                
-
-                Board.selected_piece.coords = sel_piece_pos
-                Board.selected_piece.board_pos = sel_piece_board_pos
+                Board.selected_piece.coords = new_piece_pos
+                Board.selected_piece.board_pos = new_board_pos
+                Board.holding_piece = None
                 Board.selected_piece = None
             else:
                 print(f"Piece drop to its old position")
                 Board.selected_piece.coords = board_pos_to_coords(Board.selected_piece.board_pos)
-                Board.selected_piece = None
+                Board.holding_piece = None
         else:
             print(f"Piece drop to its old position")
             Board.selected_piece.coords = board_pos_to_coords(Board.selected_piece.board_pos)
-            Board.selected_piece = None
+            Board.holding_piece = None
 
-def select_square(mouse_x: float, mouse_y: float) -> None:
-    sel_board_pos = coords_to_board_pos((mouse_x, mouse_y))
-    if sel_board_pos != -1:
-        sel_board_coords = board_pos_to_coords(sel_board_pos)
-        Board.mouse_on_square = [sel_board_pos, sel_board_coords]
+def mouse_on_square_select(mouse_x: float, mouse_y: float) -> None:
+    new_board_pos = coords_to_board_pos((mouse_x, mouse_y))
+    if new_board_pos != -1:
+        new_board_coords = board_pos_to_coords(new_board_pos)
+        Board.mouse_on_square = [new_board_pos, new_board_coords]
     else:
         Board.mouse_on_square = [-1, -1]
 
@@ -181,7 +179,8 @@ def main():
                 print("\nMouse down")
                 mouse_x, mouse_y = pygame.mouse.get_pos()
 
-                select_piece(mouse_x, mouse_y, board) 
+                board.select_piece(mouse_x, mouse_y)
+                #select_piece(mouse_x, mouse_y, board) 
 
 
             if event.type == pygame.MOUSEBUTTONUP:
@@ -194,8 +193,8 @@ def main():
             if event.type == pygame.MOUSEMOTION:
                 mouse_x, mouse_y = pygame.mouse.get_pos()
 
-                select_square(mouse_x, mouse_y)
-                selected_piece_move_floating(mouse_x, mouse_y)
+                mouse_on_square_select(mouse_x, mouse_y)
+                holding_piece_move(mouse_x, mouse_y)
 
         draw()
     
